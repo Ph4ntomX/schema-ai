f04df2b7c22a4ea935f199bff5686e2a72bfdd81
Hamza <tengkuhamza66@gmail.com>
2026-07-25 22:17:04 +0800

    Fixed runtime error

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const withPWA = withPWAInit({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-  customWorkerDir: "worker",
 });
 
 const nextConfig: NextConfig = {
